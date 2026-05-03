0346d7082aa1a6e6713786d700ab6f2dc3aaf2fd
Javier Saiz Gomez <jsg415@inlumine.ual.es>
2026-05-04 00:38:26 +0200

    feat: login

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,12 @@
+// src/app/app.routes.ts
 import { Routes } from '@angular/router';
+import { HomeLandingComponent } from './features/auth/home-landing/home-landing.component';
+import { LoginComponent } from './features/auth/login.component';
+import { RegisterComponent } from './features/auth/register.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeLandingComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: '**', redirectTo: '' }
+];
