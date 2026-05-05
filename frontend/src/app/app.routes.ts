e283218eeec6ed6b52a96c2f5a31e79d464c7062
Javier Saiz Gomez <jsg415@inlumine.ual.es>
2026-05-05 12:08:40 +0200

    forgot-password

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { Routes } from '@angular/router';
 import { HomeLandingComponent } from './features/auth/home-landing/home-landing.component';
 import { LoginComponent } from './features/auth/login.component';
 import { RegisterComponent } from './features/auth/register.component';
+import { ForgotPasswordComponent } from './features/auth/forgot-password/forgot-password.component';
 
 export const routes: Routes = [
   { path: '', component: HomeLandingComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
+  { path: 'forgot-password', component: ForgotPasswordComponent },
   { path: '**', redirectTo: '' }
 ];
